3d093364ed23730fd3467f034ec537b91da46301
PickolZi <pickol876@gmail.com>
2025-11-13 22:57:32 -0800

    modify box script to take in AWS S3 URL instead of a file. No need to store EPRs locally, can send them smartsheet's direct downloadable URL

--- box.py
+++ box.py
@@ -1,4 +1,7 @@
 import os
+import requests
+from io import BytesIO
+
 from dotenv import load_dotenv
 
 from box_sdk_gen import BoxClient, BoxDeveloperTokenAuth
@@ -21,72 +24,68 @@ load_dotenv()
 # TODO: Need paid plan to create an App token that DOESN'T EXPIRE
 DEVELOPER_TOKEN = os.getenv("BOX_ACCESS_TOKEN", "")
 
-def upload_file_to_box(file_path, folder_id='0'):
+FILE_PATH = "sample_epr.pdf"  # Replace with filename from Smartsheet
+FOLDER_ID = "345470036941"  # Replace with box com file id from URL
+
+def upload_file_to_box_by_url(s3_url, filename, folder_id=FOLDER_ID):
     """
-    Upload a file to Box.
+    Upload a file to Box through s3_url.
     
     Args:
-        file_path: Path to the file to upload
+        file_path: AWS S3 Url path to the file to upload
         folder_id: Box folder ID (default '0' is root folder)
     
     Returns:
         Uploaded file object
     """
 
+    # Get Box client
     auth: BoxDeveloperTokenAuth = BoxDeveloperTokenAuth(token=DEVELOPER_TOKEN)
     client: BoxClient = BoxClient(auth=auth)
     
-    # Does file exists?
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"File not found: {file_path}")
-    
     # Upload file to box com
-    # TODO: helper function to create custom filename
-    filename = "sample_epr_freddy.pdf"
     uploaded_files: Files | None = None
-    print(f"Uploading {file_path} to Box folder {folder_id}...")
+    print(f"Uploading {s3_url} to Box folder {folder_id}...")
     try:
-        with open(file_path, "rb") as byte_stream:
-            uploaded_files = client.uploads.upload_file(
-                attributes=UploadFileAttributes(
-                    name=filename, parent=UploadFileAttributesParentField(id=folder_id)
-                ),
-                file=byte_stream
-            )
+        response = requests.get(s3_url)
+
+        pdf_file = BytesIO(response.content)
+        uploaded_files = client.uploads.upload_file(
+            attributes=UploadFileAttributes(
+                name=filename, parent=UploadFileAttributesParentField(id=folder_id)
+            ),
+            file=pdf_file
+        )
+
     except BoxAPIError as err:
         if err.response_info.status_code == 409:
-            raise FileExistsError(f"File already exists in Box: {file_path}")
+            raise FileExistsError(f"File already exists in Box: {filename}")
     
     if not uploaded_files:
-        raise RuntimeError(f"Something failed when uploading: {file_path}")
+        raise RuntimeError(f"Something failed when uploading: {filename}")
     
     # Successfully uploaded file. Printing results
     uploaded_file = uploaded_files.entries[0] # type: ignore
-    print(f"✓ File uploaded successfully!")
-    print(f"  File ID: {uploaded_file.id}")
-    print(f"  File Name: {uploaded_file.name}")
-    print(f"  File URL: https://app.box.com/file/{uploaded_file.id}")
+    # print(f"✅ File uploaded successfully!")
+    # print(f"  File ID: {uploaded_file.id}")
+    # print(f"  File Name: {uploaded_file.name}")
+    # print(f"  File URL: https://app.box.com/file/{uploaded_file.id}")
     
     return uploaded_file
 
 
-def main():
-    FILE_PATH = "sample_epr.pdf"  # Replace with filename from Smartsheet
-    FOLDER_ID = "345470036941"  # Replace with box com file id from URL
-    
-    try:
-        uploaded_file = upload_file_to_box(FILE_PATH, FOLDER_ID)
-        print("\n✓ Upload complete!")
+# def main():
+    # try:
+    #     s3_url = ""
+    #     uploaded_file = upload_file_to_box_by_url(s3_url, FOLDER_ID)
+    #     print("\n✓ Upload complete!")
         
-    except FileNotFoundError as err:
-        print(f"Error: {err}")
-        print(f"Make sure {FILE_PATH} is in the current directory")
-    except FileExistsError as err:
-        print(f"Error: {err}")
-        print(f"Please rename or check box for {FILE_PATH} for duplicates")
-    except Exception as e:
-        print(f"Error uploading file: {e}")
+    # except FileExistsError as err:
+    #     print(f"Error: {err}")
+    #     print(f"Please rename or check box for {FILE_PATH} for duplicates")
+    # except Exception as e:
+    #     print(f"Error uploading file: {e}")
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
